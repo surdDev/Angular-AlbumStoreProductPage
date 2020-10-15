5854fead5ff30d796dabd5239ab8e9027c20a65b
surdDev <yusufsodiq22@gmail.com>
2020-10-15 23:31:05 +0100

    second commit

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {Interface} from '../album'
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
